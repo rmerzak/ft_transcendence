593b1a2dd2bae9691d98901d90f880d11b4460cc
rmerzak <rmerzak@student.1337.ma>
2023-11-18 22:38:19 +0100

    finishing login page

--- frontend/app/login/page.jsx
+++ frontend/app/login/page.jsx
@@ -1,16 +1,10 @@
 const Login = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 hero-gradient">
       <div className="flex flex-col items-center justify-center w-full flex-1 px-5 md:px-20 text-center">
         <div className="bg-white rounded-2xl shadow-2xl flex flex-col md:flex-row w-full max-w-4xl">
-          <div className="w-full md:w-3/5 p-5">
-          <p>3d section</p>
-          <p>3d section</p>
-          <p>3d section</p>
-          <p>3d section</p>
-          <p>3d section</p>
-          <p>3d section</p>
-          <p>3d section</p>
+          <div className="flex items-center w-full md:w-3/5 p-5">
+            <img src="/game.gif" alt="game" />
             </div>
           <div className="w-full md:w-2/5 bg-green-500 text-white rounded-t-2xl  rounded-b-2xl md:rounded-tr-2xl  py-5 md:py-36 px-5 md:px-12">
             <h2 className="text-3xl font-bold mb-2">Welcome, Player!</h2>
