584ff13ffaff5ed736016248d6e091b6f85fcf2f
rmerzak <rmerzak@student.1337.ma>
2023-11-24 21:24:33 +0100

    navbar+sidbar finished

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -6,11 +6,11 @@ const Layout = ({children}) => {
       <div className="flex login-gradient">
         <div className="flex-1 ">
             <Sidebar>
-              <SidebarItem icon={<Home color="#ffff" size={20}/>} to="/dashboard/home" text="Home"/>
-              <SidebarItem icon={<User color="#ffff" size={20}/>} to="/dashboard/profile" text="Profile"  />
-              <SidebarItem icon={<MessageCircle color="#ffff" size={20}/>} to="/dashboard/chat" text="Chat"/>
-              <SidebarItem icon={<BookUser color="#ffff" size={20}/>} to="/dashboard/friends" text="Friends" />
-              <SidebarItem icon={<Gamepad2 color="#ffff" size={20}/>} to="/dashboard/game" text="Game" />
+              <SidebarItem icon={<Home color="#ffff" size={25}/>} to="/dashboard/home" text="Home"/>
+              <SidebarItem icon={<User color="#ffff" size={25}/>} to="/dashboard/profile" text="Profile"  />
+              <SidebarItem icon={<MessageCircle color="#ffff" size={25}/>} to="/dashboard/chat" text="Chat"/>
+              <SidebarItem icon={<BookUser color="#ffff" size={25}/>} to="/dashboard/friends" text="Friends" />
+              <SidebarItem icon={<Gamepad2 color="#ffff" size={25}/>} to="/dashboard/game" text="Game" />
             </Sidebar>
         </div>
         <div className="flex-[10] ">
--- frontend/app/ui/dashboard/sidebar/sidebar.tsx
+++ frontend/app/ui/dashboard/sidebar/sidebar.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import Link from 'next/link';
-import { BellDot, ChevronFirst, ChevronLast, MoreVertical } from 'lucide-react';
+import {  ChevronLeft, ChevronRight } from 'lucide-react';
 import { useState } from 'react';
-import Image from 'next/image';
 import { useContext, createContext } from "react"
 import { usePathname } from 'next/navigation';
 
@@ -12,30 +11,19 @@ const SidebarContext = createContext();
 const Sidebar = ({children}) => {
   const [expanded, setExpanded] = useState(true);
     return (
-      <aside className={`h-screen shadow-xl bg-fuchsia-900/50 ${expanded ? "w-fill" : "md:w-[90px] w-[60px]"}` }>
+      <aside className={`h-screen shadow-lg shadow-[#78196F]  bg-fuchsia-900/50 ${ expanded ? "w-40" : "md:w-20 w-15" }` }>
         <nav className={`h-full w-fill flex flex-col  `}>
             <div className='flex items-center justify-between mt-8 mb-32 scroll-pl-6 '>
-              <img src="/pingsvg.svg" alt="42" className={`overflow-hidden transition-all ${expanded ? "w-10" : "w-0" }`} />
+              <img src="/pingsvg.svg" alt="42" className={`overflow-hidden transition-all ${expanded ? "w-10" : "w-0 " }`} />
               <span className={`text-white font-bold ${expanded ? "w-20" : "hidden" }`}>PingPong</span>
               <div onClick={() => setExpanded(curr => !curr)} className='p-1 rounded-lg  bg-gray-50 hover:bg-slate-100 cursor-pointer'>
-                {expanded ? <ChevronFirst size={20}/> : <ChevronLast size={20}/>}
+                {expanded ? <ChevronLeft size={20} className='rounded-full'/> : <ChevronRight size={20}/>}
               </div>
             </div>
 
           <SidebarContext.Provider value={{ expanded }}>
           <ul className="flex-col px-3">{children}</ul>
         </SidebarContext.Provider>
-          {/* <div className='border-t flex p-3'>
-            <img src="/profile1.png" alt="foto" className="w-10 h-10 rounded-md"/>
-            <div className={` flex justify-between items-center overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}`}>
-              <div className='leading-4 text-white'>
-                <h4 className='font-bold text-sm'>John Doe</h4>
-                <p className='text-xs '>Player | Email</p>
-              </div>
-              <BellDot color="#ffff" size={20}/>
-              <MoreVertical color="#ffff" size={20}/>
-            </div>
-          </div> */}
         </nav>
       </aside>
     )
@@ -46,13 +34,12 @@ export default Sidebar
 export function SidebarItem({ icon, text, to }) {
   const currentRoute = usePathname();
   const { expanded } = useContext(SidebarContext)
-  console.log(currentRoute)
   return (
     <>
     <Link href={to}  >   
-    <li className={` ${currentRoute === to ? " bg-[#78196F]" : "text-white"} relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group text-white `}>
+    <li className={` ${currentRoute === to ? " bg-[#78196F]" : "text-white"} relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group text-white ${ expanded ? "w-35 ml-3" : "w-15" }`}>
       {icon}
-      <span className={` overflow-hidden transition-all  ${ expanded ? "w-52 ml-3" : "w-0" }`}>
+      <span className={` overflow-hidden transition-all  ${ expanded ? "w-35 ml-3" : "w-0" }`}>
         {text}
       </span>
       {!expanded && (
