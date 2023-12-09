3ea9ae4425dcd9b73ba716954cfec36b151522b4
zarran <zarran@kali.home>
2023-12-09 17:42:27 +0100

    the second page i think it's finished bi idni lah

--- frontend/src/components/game/Online.tsx
+++ frontend/src/components/game/Online.tsx
@@ -1,36 +1,92 @@
 'use client';
 import { Link } from 'lucide-react'
 import styles from '../../app/dashboard/game/page.module.css'
-import React from 'react';
-import { SketchPicker } from 'react-color';
+import React, { useState } from 'react';
 import Image from 'next/image'
+import { CirclePicker } from 'react-color';
 
 
 function Online() {
+
+  const [color, setColor] = useState('#ffffff');
+  const handleChangeComplete = (color: { hex: React.SetStateAction<string>; }) => {
+    setColor(color.hex);
+  }
+
+  const [selectedOption, setSelectedOption] = useState('paddle');
+
+  const handleOptionChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
     <>
         <div className={styles.title}>
             <h1>Game</h1>
         </div>
         <div className={styles.container_for_content}>
+
             <div className={styles.content_left}>
-                <h2 className={styles.typeOfGame}>Preview</h2>
                 <div className={styles.game_preview}>
-                    <Image
-                        className={styles.preview_img}
-                        src="/pong.jpg"
-                        alt="pong"
-                        width={500}
-                        height={500}
-                        draggable="false"
-                        />
-                </div>
+                  <h2 className={styles.typeOfGame}>Preview</h2>
+                      <div className={styles.game_preview_div} style={{ backgroundColor: color}}>
+                         
+                      </div>
+                  </div>
             </div>
+
             <div className={styles.content_right}>
-                <h2 className={styles.typeOfGame}>Customize</h2>
-                <SketchPicker />
-            </div>
+                <div className={styles.color_picker}>
+                  <h2 className={styles.typeOfGame}>Customize</h2>
                 
+                  <CirclePicker
+                      color={ color }
+                      onChangeComplete={ handleChangeComplete } 
+                  />
+                </div>
+
+                <div className={styles.color_picker}>
+                  <h2 className={styles.typeOfGame}>Choose</h2>
+                
+                  <div className={styles.radio_div}>
+                    <label className={styles.label_op}>
+                      <input
+                        className={styles.radio_input}
+                        type="radio"
+                        value="paddle"
+                        checked={selectedOption === 'paddle'}
+                        onChange={handleOptionChange}
+                      />
+                      Paddle
+                    </label>
+
+                    <label className={styles.label_op}>
+                      <input
+                        className={styles.radio_input}
+                        type="radio"
+                        value="ball"
+                        checked={selectedOption === 'ball'}
+                        onChange={handleOptionChange}
+                      />
+                      Ball
+                    </label>
+
+                    <label className={styles.label_op}>
+                      <input
+                        className={styles.radio_input}
+                        type="radio"
+                        value="table"
+                        checked={selectedOption === 'table'}
+                        onChange={handleOptionChange}
+                      />
+                      Table
+                    </label>
+
+                  </div>
+                </div>
+
+            </div>
+ 
         </div>
     </>
     );
