0c3679bdc600eb57a5e0f3e59cfc6c051c225fc0
zarran <zarran@kali.home>
2023-12-09 01:20:29 +0100

    bulid the first page of the game

--- frontend/src/app/dashboard/game/page.tsx
+++ frontend/src/app/dashboard/game/page.tsx
@@ -1,10 +1,16 @@
+import styles from './page.module.css';
+import OnlineOrBot from '@/components/game/OnlineOrBot';
+import dynamic from 'next/dynamic';
+// const OnlineOrBot = dynamic(() => import('@/components/game/OnlineOrBot'), { ssr: false })
+import React from 'react';
+
 
 const Game = () => {
-    return (
-      <div>
-        Game
-      </div>
-    )
-  }
-  
-  export default Game
+  return (
+    <div className={styles.game_container}>
+      <OnlineOrBot />
+    </div>
+  );
+};
+
+export default Game;
