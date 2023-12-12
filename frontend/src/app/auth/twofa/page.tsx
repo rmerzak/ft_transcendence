95ef5c25b575e0ee5a5052005f944505aa7148fe
rmerzak <rmerzak@student.1337.ma>
2023-12-12 11:58:01 +0100

    updating 2fa front

--- frontend/src/app/auth/twofa/page.tsx
+++ frontend/src/app/auth/twofa/page.tsx
@@ -5,7 +5,7 @@ const Verify = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 login-gradient">
       <div className="flex flex-col items-center justify-center w-full flex-1 px-5 md:px-20 text-center">
-        <div className="text-white font-bold text-[48px]">Two Authentication Factor</div>
+        <div className="text-white font-bold text-[48px]">Two-factor authentication</div>
         <TwoFa />
         <div className="flex pt-5 items-center flex-col">
           <div>
--- frontend/src/components/auth/TwoFa.tsx
+++ frontend/src/components/auth/TwoFa.tsx
@@ -3,6 +3,7 @@
 import { color } from "framer-motion";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { QrCode } from "lucide-react";
 const TwoFa = () => {
     const [qrCodeImage, setQrCodeImage] = useState('');
 
@@ -27,13 +28,15 @@ const TwoFa = () => {
 
     return (
         <div className="bg-[#311251] drop-shadow-2xl w-[380px] md:w-[500px] bg-opacity-50 pb-10 rounded-2xl  flex items-center justify-center flex-col max-w-4xl">
-            <div className="text-white text-[20px]" style={{marginBottom: "10px", marginTop: "10px"}}>Please scan the QR code below</div>
+            <QrCode size={100} color="#fff" />
+            <div className="text-white text-[20px]" style={{marginBottom: "10px", marginTop: "10px"}}>Please enter 2FA code</div>
+            <div className="text-white font-extralight text-[14px]" style={{marginBottom: "10px", marginTop: "10px"}}>Two-factor authentication (2FA) is enable for your account. <br /> Please enter a code to log in.</div>
             <div>
                 <img draggable="false" src={qrCodeImage} alt="" className="w-[200px]"/>
             </div>
             <div className="flex items-center bg-white mt-6 border-[0.063rem] rounded-[1rem] overflow-hidden relative ">
         <label htmlFor="name"></label>
-        <input type="text" id="name" placeholder="code"  className="w-[20.438rem] h-[2.75rem] pl-[1.063rem] leading-normal" />
+        <input type="text" id="name" placeholder="Two-factor authentication code"  className="w-[20.438rem] h-[2.75rem] pl-[1.063rem] leading-normal" />
       </div>
       <div className="pt-5 flex items-center justify-between w-[20.438rem] h-[2.75rem]" style={{marginBottom: "10px", marginTop: "10px"}}>
         <button  className="bg-[#79196F]  w-[100px] h-[40px] text-white py-2 px-4 rounded-[10px] border-colors">Back</button>
