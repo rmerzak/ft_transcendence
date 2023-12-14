4ea07ec2a1aa67382ff18f9aebce84f3dedc8cf4
ANAS-AAMMARI <aaammari@student.1337.ma>
2023-12-14 14:41:16 +0100

    chat is loading...

--- frontend/src/app/dashboard/chat/page.tsx
+++ frontend/src/app/dashboard/chat/page.tsx
@@ -4,6 +4,7 @@ import Message from "@/components/chat/msg";
 import MsgShow from "@/components/chat/msgshow";
 
 const Chat = () => {
+  
   return (
     <div className="w-full bg-[#311251]/80 md:rounded-3xl rounded-t-md md:w-[95%] md:h-[90%] md:mt-6 md:overflow-auto md:mx-auto md:shadow-lg">
       <h1 className="text-white md:text-2xl text-lg md:font-bold text-center m-2 p-1 md:m-4 md:p-2 font-inter w-full">
