7abfee0c4b8f5ec48a56255aa9dddb7a7db3d99b
Yassine Moutaouakil <ymoutaou@e1r6p9.1337.ma>
2023-12-18 12:59:15 +0100

    start with backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,11 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { MulterModule } from '@nestjs/platform-express';
+import { GameModule } from './game/game.module';
 
 
 @Module({
-  imports: [ AuthModule, UserModule, PrismaModule, ConfigModule.forRoot({isGlobal:true,}), MulterModule.register({dest : './uploads'})],
+  imports: [ AuthModule, UserModule, PrismaModule, ConfigModule.forRoot({isGlobal:true,}), MulterModule.register({dest : './uploads'}), GameModule],
   controllers: [],
   providers: [],
 })
