fc6363b202c98abbff4ad4f7e867f6c3fdc30e01
ANAS-AAMMARI <aaammari@student.1337.ma>
2024-01-18 16:28:22 +0100

    user to user done

--- frontend/src/components/chat/msg/emoji/emojiPicker.tsx
+++ frontend/src/components/chat/msg/emoji/emojiPicker.tsx
@@ -14,17 +14,9 @@ const EmojiPicker: React.FC<EmojiPickerProps> = ({ setMessage }) => {
 
     return (
         <>
-            <Image
-                tabIndex={0}
-                src="/emoji.svg"
-                alt="emoji"
-                width={20}
-                height={20}
-                draggable={false}
-                onClick={() => setShowEmojiPicker(!showEmojiPicker)}
-                className="cursor-pointer h-[50%] w-[10%]"
-                // style={{ width: "auto", height: "auto" }}
-            />
+            <button onClick={() => setShowEmojiPicker(!showEmojiPicker)}>
+                <span className='text-2xl'>&#128512;</span>
+            </button>
             {showEmojiPicker &&
                 <div className="absolute bottom-[9%] right-[39.48%] z-10 w-[11%] overflow-y-auto rounded-lg bg-[#311150]/40 border">
                     <div className="flex flex-wrap justify-center max-h-80">
