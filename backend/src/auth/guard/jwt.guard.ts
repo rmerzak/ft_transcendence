4f14c7a1c7813bc1156053d3213ded668d9c9d16
rmerzak <rmerzak@student.1337.ma>
2024-01-18 14:34:14 +0100

    prepare to merge with game

--- backend/src/auth/guard/jwt.guard.ts
+++ backend/src/auth/guard/jwt.guard.ts
@@ -4,41 +4,24 @@ import { JwtService } from "@nestjs/jwt";
 import { verify } from "jsonwebtoken";
 @Injectable()
 export class JwtGuard extends AuthGuard('jwt') {
-    constructor(private jwtService: JwtService) {
+    constructor() {
         super();
     }
-    //  async canActivate(context: ExecutionContext): Promise<boolean>{
-    //     //  const request = context.switchToHttp().getRequest();
-    //     //  const response = context.switchToHttp().getResponse();
-    //     //  const token = request.cookies['accesstoken'];
-    //     //  if (token) {
-    //     //      const payload =  this.jwtService.decode(token);
-    //     //      if (payload['sub'].toString() != request.cookies['userId']) {
-    //     //         console.log("JwtGuard")
-    //     //         response.clearCookie('accesstoken');
-    //     //         response.clearCookie('userId')
-    //     //         return false;
-                
-    //     //     }
-    //     // }
-        
-    //     return super.canActivate(context) as boolean;
-    //   }
-    //  async canActivate(context: ExecutionContext): Promise<boolean>{
-    //      const request = context.switchToHttp().getRequest();
-    //      const response = context.switchToHttp().getResponse();
-    //      const token = request.cookies['accesstoken'];
-    //      if (token) {
-    //         try {
-    //             const payload = verify(token, process.env.JWT_SERCRET);
-    //             return super.canActivate(context) as boolean;
-    //         } catch (error) {
-    //             response.clearCookie('accesstoken');
-    //             response.clearCookie('userId');
-    //             throw new UnauthorizedException('Invalid or expired token');
-    //         }
-    //     }
-    //     return false;
-    //   }
-      
+     async canActivate(context: ExecutionContext) : Promise<boolean>{
+         const request = context.switchToHttp().getRequest();
+         const response = context.switchToHttp().getResponse();
+         const token = request.cookies['accesstoken'];
+         if (token) {
+            try{
+                const payload = verify(token, process.env.JWT_SERCRET);
+                console.log(payload)
+                return super.canActivate(context) as boolean;
+            } catch (error) {
+                response.clearCookie('accesstoken');
+                response.clearCookie('userId');
+                throw new UnauthorizedException('Invalid or expired token');
+            }
+        }
+    
+    } 
 }
