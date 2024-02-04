da92b52f716c60513da6b8db77759081bfb5bf9e
ymoutaou <live.zaran@gmail.com>
2024-02-04 17:14:46 +0100

    working on leaderboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,38 +1,61 @@
 'use client'
 import { ContextGlobal } from '@/context/contex';
-import React, { useContext } from 'react'
+import React, { use, useContext, useEffect, useState } from 'react'
 import { Eye } from 'lucide-react';
 import LeaderboardEntry from '@/components/LeaderBoard/LeaderBorditem';
 import RankEntre from '@/components/LeaderBoard/RankEntre';
+import Loading from '@/components/game/Loading';
 
+type LesderboardEntry = {
+  gameElo: number;
+  gameMatches: number;
+  gameRank: number;
+  gameWins: number;
+  id: number;
+  image: string;
+  username: string;
+}
 
 const Dashboard = () => {
-  const leaderboardData = [
-    { rank: 1, avatarSrc: '/mberri.png', smallAvatarSrc: '/Pandora.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 2, avatarSrc: '/rabi.png', smallAvatarSrc: '/commodor.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 3, avatarSrc: '/people-01.png', smallAvatarSrc: '/freax.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 4, avatarSrc: '/people-01.png', smallAvatarSrc: '/Bios.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 5, avatarSrc: '/mberri.png', smallAvatarSrc: '/freax.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 6, avatarSrc: '/people-02.png', smallAvatarSrc: '/Bios.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 7, avatarSrc: '/people-01.png', smallAvatarSrc: '/freax.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 8, avatarSrc: '/people-03.png', smallAvatarSrc: '/Pandora.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 9, avatarSrc: '/people-02.png', smallAvatarSrc: '/freax.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 10, avatarSrc: '/mberri.png', smallAvatarSrc: '/freax.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 11, avatarSrc: '/mberri.png', smallAvatarSrc: '/Bios.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 12, avatarSrc: '/people-01.png', smallAvatarSrc: '/freax.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 13, avatarSrc: '/people-02.png', smallAvatarSrc: '/commodor.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 14, avatarSrc: '/mberri.png', smallAvatarSrc: '/freax.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 15, avatarSrc: '/people-03.png', smallAvatarSrc: '/Pandora.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 16, avatarSrc: '/mberri.png', smallAvatarSrc: '/Bios.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 17, avatarSrc: '/people-02.png', smallAvatarSrc: '/freax.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 18, avatarSrc: '/people-03.png', smallAvatarSrc: '/freax.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 19, avatarSrc: '/rabi.png', smallAvatarSrc: '/commodor.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 20, avatarSrc: '/people-03.png', smallAvatarSrc: '/freax.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 21, avatarSrc: '/mberri.png', smallAvatarSrc: '/Pandora.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-    { rank: 22, avatarSrc: '/people-03.png', smallAvatarSrc: '/Bios.png', username: 'UserGuest123', matchesPlayed: 100, wins: 500, ratio: '50%' },
-  ];
+
+  const { profile }: any = useContext(ContextGlobal);
+  const [leaderboardData, setLeaderboardData] = useState<LesderboardEntry[]>([]);
+  const [rank, setRank] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchLeaderboardData = async () => {
+    try {
+      setLoading(true);
+      // use fetch
+      const res = await fetch(`${process.env.API_BASE_URL}/api/leaderboard`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+      const data = await res.json();
+      setLeaderboardData(data.leaderboard);
+      data.leaderboard.forEach((entry: LesderboardEntry) => {
+        if (entry.id === profile.id) {
+          setRank(entry.gameRank);
+        }
+      });
+    } catch {}
+    finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (profile.id !== -1) {
+      fetchLeaderboardData();
+    }
+  }, [profile.id]);
+
   return (
     <div className="bg-profile h-screen m-4 py-4 pl-4 md:pr-0 pr-2 backdrop-blur-md">
+      <Loading isLoading={loading} />
      <h1 className="text-white font-bold text-3xl text-center mt-6 mb-8">LeaderBoard</h1>
      <div className='flex flex-row'>
      <div className='w-[70%]'>
@@ -41,25 +64,26 @@ const Dashboard = () => {
           <h1>Player</h1>
           <h1>Matches</h1>
           <h1>Wins</h1>
-          <h1>Ratio</h1>
+          <h1>Score</h1>
           <h1>Profile</h1>
         </div>
         <div className='h-[720px] overflow-auto w-full'>
         {leaderboardData.map((entry, index) => (
           <LeaderboardEntry
             key={index}
-            rank={entry.rank}
-            avatarSrc={entry.avatarSrc}
-            smallAvatarSrc={entry.smallAvatarSrc}
+            rank={entry.gameRank}
+            avatarSrc={entry.image}
+            smallAvatarSrc="/Pandora.png"
             username={entry.username}
-            matchesPlayed={entry.matchesPlayed}
-            wins={entry.wins}
-            ratio={entry.ratio}
+            matchesPlayed={entry.gameMatches}
+            wins={entry.gameWins}
+            score={entry.gameElo}
+            profile={entry.username}
           />
         ))}
         </div>
         </div>
-        <RankEntre rank={1} avatarSrcWinner='/mberri.png' avatarSrcSecond='/people-03.png' avatarSrcThird='/rabi.png' bestRank={1}/>
+        {/* <RankEntre rank={rank} avatarSrcWinner={!loading ? leaderboardData[0].image : ''} avatarSrcSecond={!loading ? leaderboardData[1].image : ''} avatarSrcThird='/rabi.png' bestRank={1}/> */}
      </div>
     </div>
   )
