1dde6e8ffef93d23f8146b9a32397183ff328c43
ymoutaou <live.zaran@gmail.com>
2024-02-17 16:28:36 +0100

    fix some error

--- frontend/src/components/game/Bot.tsx
+++ frontend/src/components/game/Bot.tsx
@@ -334,7 +334,7 @@ function Bot() {
                 updateScores(user.score, com.score);
                 resetBall();
             }
-            if (user.score == 5 || com.score == 5) {
+            if (user.score == 500 || com.score == 500) {
                 clearInterval(intervalId);
                 Swal.fire({
                     title: 'Game Over',
